1fb7924493f5ecc4b70a13cb38ac42529031321c
AdamRat <34485945+AdamRat@users.noreply.github.com>
2022-05-16 20:08:56 +0200

    Add files via upload

--- Programowanie zaawansowane zaliczenie/Controllers/ContactController.cs
+++ Programowanie zaawansowane zaliczenie/Controllers/ContactController.cs
@@ -2,47 +2,46 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using Programowanie_zaawansowane_zaliczenie.Models;
+using System.Linq;
 
 namespace Programowanie_zaawansowane_zaliczenie.Controllers
 {
     public class ContactController : Controller
     {
-        private static IList<ContactVievModel> tasks = new List<ContactVievModel>()
+        private static IList<ContactVievModel> Contacts = new List<ContactVievModel>()
         {
-            new ContactVievModel(){ Id=1, FirstName= "Wizyta u lekarza", LastName= "Godzina 17:00", PhoneNumber=false },
-            new ContactVievModel(){ TaskId=2, Name= "Zrobić obiad", Description= "Pierogi", Done=false },
+            new ContactVievModel(){ Id=1, FirstName= "Wizyta u lekarza", LastName= "Godzina 17:00", 
+                Adress=new Adresses(){Id=1,Street="tarnowska",PostCode="61-323", City="poznan", Country="polsza" },
+            PhoneNumber="722090231", ContactCategory=new ContactCategories(){Id=1,CategoryName="test" },FbLink="https://test.pl", Email="test@test.pl" },
+            new ContactVievModel(){ Id=2, FirstName= "Wizyta u lekarza", LastName= "Godzina 17:00",
+                Adress=new Adresses(){Id=2,Street="tarnowska2",PostCode="61-3232", City="poznan2", Country="polsza2" },
+            PhoneNumber="722090231", ContactCategory=new ContactCategories(){Id=2,CategoryName="test2" },FbLink="https://test.pl", Email="test@test.pl" },
         };
         // GET: ContactController
         public ActionResult Index()
         {
-            return View();
+            return View(Contacts);
         }
 
         // GET: ContactController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            return View(Contacts.FirstOrDefault(x=>x.Id==id));
         }
 
         // GET: ContactController/Create
         public ActionResult Create()
         {
-            return View();
+            return View(new ContactVievModel());
         }
 
         // POST: ContactController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(ContactVievModel collection)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            Contacts.Add(collection);
+            return RedirectToAction(nameof(Index));
         }
 
         // GET: ContactController/Edit/5
